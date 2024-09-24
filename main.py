aab14adeb3adcfe79c6ef87951092fdb0e407e81
JCarlos <jcarloscm9312@gmail.com>
2024-09-24 07:12:56 -0500

    Creamos el archivo principal para el inicio del desarrollo del proyecto

---
+++ main.py
@@ -0,0 +1,11 @@
+# Este es el archivo principal
+
+def suma(a, b):
+    return a + b
+
+def main():
+    print("Operaciones matemáticas")
+    print("Suma: ", suma(5, 3))
+    
+if __name__ == "__main__":
+    main()
